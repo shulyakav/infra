54299e3a962ffe71361e9a90c6aeb7384374c141
shulyakav <shulyakav@tera1.c.infra-213220.internal>
2018-08-14 09:51:29 +0000

    add sh file in parcker section

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+provider "google" {
+         project = "infra-213220"
+         region = "europe-west1"
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+name = "allow-puma-default"
+# Название сети, в которой действует правило
+network = "default"
+# Какой доступ разрешить
+allow {
+protocol = "tcp"
+ports = ["9292"]
+}
+# Каким адресам разрешаем доступ
+source_ranges = ["0.0.0.0/0"]
+# Правило применимо для инстансов с тегом …
+target_tags = ["reddit-app"]
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-1534196180"
+    }
+  }
+
+  metadata {
+    sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
+  }
+
+  tags = ["reddit-app"]
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file("~/.ssh/appuser")}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}

3835977b8bd568ddc54d52fdd0c9df2397941e1e
shulyakav <shulyakav@gmail.com>
2018-08-14 10:12:39 +0000

    remove key form install_ruby.sh

--- packer-base/scripts/install_ruby.sh
+++ packer-base/scripts/install_ruby.sh
@@ -2,7 +2,7 @@
 
 
 ####ruby
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 
 
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
